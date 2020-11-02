a209fc5abf3e5a82f73bd3d540c43b973e333210
faraz ali khan <farazalikhan33@gmail.com>
2020-11-02 12:54:54 +0530

    Update config.py
    Hidden config Details.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,10 @@
 
 class Config:
 
-    SECRET_KEY = '0f90c68bb1efb63bd921e5ba72000d16'
+    SECRET_KEY = '' # the secret key
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = 'flaskblogger@gmail.com'
-    MAIL_PASSWORD = 'newhorizons1A'
+    MAIL_PASSWORD = '' # the password
